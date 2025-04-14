59283837ef2cb8d935b2e0c2909898cf04326085
Albert <337144986@qq.com>
2025-04-14 19:10:39 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+ECHO 处于打开状态。
+javascript
+function add(a, b) {
+    return a + b;
+}
+
+module.exports = add;
---
+++ app.test.js
@@ -0,0 +1,10 @@
+ECHO 处于打开状态。
+javascript
+const add = require('../app');
+const { expect } = require('chai');
+
+describe('Add function', () => {
+    it('should return 5 when adding 2 and 3', () => {
+        expect(add(2, 3)).to.equal(5);
+    });
+});
